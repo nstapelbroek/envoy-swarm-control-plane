06dcb2254eb53117688497dcc1c1fe86a15ac4c2
Nico Stapelbroek <nstapelbroek@gmail.com>
2020-06-27 16:57:43 +0200

    wip creating channels for conversion

--- internal/docker/provider.go
+++ internal/docker/provider.go
@@ -5,10 +5,15 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/client"
+	envoy_config_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 )
 
 type SwarmProvider struct {
 	dockerClient client.APIClient
+	cdsConversions <-chan *swarm.Service
+	edsConversions <-chan *swarm.Service
+	rdsConversions <-chan *swarm.Service
+	tdsConversions <-chan *swarm.Service
 }
 
 func NewSwarmProvider() SwarmProvider {
@@ -24,10 +29,67 @@ func NewSwarmProvider() SwarmProvider {
 		panic(err)
 	}
 
-	return SwarmProvider{dockerClient: c}
+	return SwarmProvider{
+		dockerClient: c,
+		cdsConversions: make(<-chan swarm.Service),
+		edsConversions: make(<-chan swarm.Service),
+		rdsConversions: make(<-chan swarm.Service),
+		tdsConversions: make(<-chan swarm.Service),
+	}
 }
 
 // ListServices will convert swarm service definitions to our own Models
 func (s SwarmProvider) ListServices(ctx context.Context) ([]swarm.Service, error) {
 	return s.dockerClient.ServiceList(ctx, types.ServiceListOptions{})
+}
+
+func (s SwarmProvider) convertServicesToClusters(services []swarm.Service) interface{} {
+	for _, s := range services {
+		cdsConversions <- &s
+		edsConversions <- &s
+		rdsConversions <- &s
+		tdsConversions <- &s
+		// todo sds for certificates
+	}
+	return envoy_config_cluster_v3.Cluster{
+		TransportSocketMatches:          nil,
+		Name:                            "",
+		AltStatName:                     "",
+		ClusterDiscoveryType:            nil,
+		EdsClusterConfig:                nil,
+		ConnectTimeout:                  nil,
+		PerConnectionBufferLimitBytes:   nil,
+		LbPolicy:                        0,
+		LoadAssignment:                  nil,
+		HealthChecks:                    nil,
+		MaxRequestsPerConnection:        nil,
+		CircuitBreakers:                 nil,
+		UpstreamHttpProtocolOptions:     nil,
+		CommonHttpProtocolOptions:       nil,
+		HttpProtocolOptions:             nil,
+		Http2ProtocolOptions:            nil,
+		TypedExtensionProtocolOptions:                 nil,
+		DnsRefreshRate:                                nil,
+		DnsFailureRefreshRate:                         nil,
+		RespectDnsTtl:                                 false,
+		DnsLookupFamily:                               0,
+		DnsResolvers:                                  nil,
+		UseTcpForDnsLookups:                           false,
+		OutlierDetection:                              nil,
+		CleanupInterval:                               nil,
+		UpstreamBindConfig:                            nil,
+		LbSubsetConfig:                                nil,
+		LbConfig:                                      nil,
+		CommonLbConfig:                                nil,
+		TransportSocket:                               nil,
+		Metadata:                                      nil,
+		ProtocolSelection:                             0,
+		UpstreamConnectionOptions:                     nil,
+		CloseConnectionsOnHostHealthFailure:           false,
+		IgnoreHealthOnHostRemoval:                     false,
+		Filters:                                       nil,
+		LoadBalancingPolicy:                           nil,
+		LrsServer:                                     nil,
+		TrackTimeoutBudgets:                           false,
+	}
 }
