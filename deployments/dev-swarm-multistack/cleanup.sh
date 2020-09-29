6049fdf28bc48bcb3d8449876baef7378c1db621
Nico Stapelbroek <nstapelbroek@gmail.com>
2020-09-29 22:34:03 +0200

    Remove unused deployment configurations
    As we add more and more complexity, I realised that I didn't use these setups anymore and they probably do not work anymore.

--- deployments/dev-swarm-multistack/cleanup.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-docker stack rm envoy myapi myfrontend
-docker network rm edge-traffic
--- deployments/dev-swarm-multistack/deploy.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-docker network create --driver=overlay --attachable edge-traffic
-docker stack deploy --compose-file ./envoy-stack.yml envoy
-docker stack deploy --compose-file ./api-stack.yml myapi
-docker stack deploy --compose-file ./frontend-stack.yml myfrontend
