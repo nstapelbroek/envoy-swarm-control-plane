dc80b250a13e1866a0875488634ee4876bdc7033
Nico Stapelbroek <nstapelbroek@gmail.com>
2020-11-24 16:11:43 +0100

    Rename folder lb to docker

--- deployments/swarm-hostmode/build.sh
+++ deployments/swarm-hostmode/build.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t nstapelbroek/envoy-edge:0.15 "$DIR"/envoy/lb
+docker build -t nstapelbroek/envoy-edge:0.15 "$DIR"/envoy/docker
 docker buildx build -t nstapelbroek/envoy-swarm-control-plane:0.15 "$DIR/../../"
